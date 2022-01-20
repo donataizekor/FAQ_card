29d13f12fbcb358f72c875d40549d60b726687ba
donataizekor <donataizekor@gmail.com>
2022-01-20 15:25:48 +0000

    added comments

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const section = document.getElementsByClassName('section');
 
 for (i=0; i<section.length; i++) {
   section[i].addEventListener('click', function () {
-    this.classList.toggle('active')    
+    this.classList.toggle('active') // adds class 'active' when elements with class 'section' gets clicked on
   })
 }
+
+
